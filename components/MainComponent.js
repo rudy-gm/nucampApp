0616f759eda4e43d2cdbab562c433f40ae74492c
rodolfo-gm <gutierrezrodolfo80@gmail.com>
2022-04-05 16:19:41 -0400

    second commit // components part 1 and 2

---
+++ components/MainComponent.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import Directory from "./DirectoryComponent";
+import { View } from "react-native";
+import CampsiteInfo from "./CampsiteInfoComponent";
+import { CAMPSITES } from "../shared/campsites";
+
+class Main extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      campsites: CAMPSITES,
+      selectedCampsite: null,
+    };
+  }
+
+  onCampsiteSelect(campsiteId) {
+    this.setState({
+      selectedCampsite: campsiteId,
+    });
+  }
+
+  render() {
+    return (
+      <View style={{flex: 1}}>
+        <Directory
+          campsites={this.state.campsites}
+          onPress={(campsiteId) => this.onCampsiteSelect(campsiteId)}
+        ></Directory>
+        <CampsiteInfo
+          campsite={
+            this.state.campsites.filter(
+              (campsite) => campsite.id === this.state.selectedCampsite
+            )[0]
+          }
+        ></CampsiteInfo>
+      </View>
+    );
+  }
+}
+
+export default Main;

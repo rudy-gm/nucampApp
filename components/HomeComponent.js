998dd72093fd00cf6b231c434a596d9e66fe3715
rodolfo-gm <gutierrezrodolfo80@gmail.com>
2022-04-05 23:00:13 -0400

    5th commit // home component

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,21 +1,61 @@
-import React, {Component} from "react";
-import { View, Text } from "react-native";
+import React, { Component } from "react";
+import { View, Text, ScrollView } from "react-native";
+import { Card } from "react-native-elements";
+import { CAMPSITES } from "../shared/campsites";
+import { PROMOTIONS } from "../shared/promotions";
+import { PARTNERS } from "../shared/partners";
 
-class Home extends Component{
+function RenderItem({ item }) {
+  if (item) {
+    return (
+      <Card
+        featuredTitle={item.name}
+        image={require("./images/react-lake.jpg")}
+      >
+        <Text style={{ margin: 10 }}>{item.description}</Text>
+      </Card>
+    );
+  }
 
-    static navigationOptions ={
-        title: 'Home'
-    }
+  return <View></View>;
+}
 
-    render(){
+class Home extends Component {
+  constructor(props) {
+    super(props);
 
-        return(
-            <View>
-                <Text>Home Component</Text>
-            </View>
-        )
-    }
+    this.state = {
+      campsites: CAMPSITES,
+      promotions: PROMOTIONS,
+      partners: PARTNERS,
+    };
+  }
+
+  static navigationOptions = {
+    title: "Home",
+  };
+
+  render() {
+    return (
+      <View>
+        <ScrollView>
+          <RenderItem
+            item={
+              this.state.campsites.filter((campsite) => campsite.featured)[0]
+            }
+          ></RenderItem>
+          <RenderItem
+            item={
+              this.state.promotions.filter((promotion) => promotion.featured)[0]
+            }
+          ></RenderItem>
+          <RenderItem
+            item={this.state.partners.filter((partner) => partner.featured)[0]}
+          ></RenderItem>
+        </ScrollView>
+      </View>
+    );
+  }
 }
 
 export default Home;
-

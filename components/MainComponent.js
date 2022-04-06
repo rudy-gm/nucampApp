f3d52f4eec23a2fa78c29ce9eb19758fa2e86d0c
rodolfo-gm <gutierrezrodolfo80@gmail.com>
2022-04-05 20:15:41 -0400

    third commit // navigation part 1

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,41 +1,43 @@
 import React, { Component } from "react";
 import Directory from "./DirectoryComponent";
-import { View } from "react-native";
+import { View, Platform } from "react-native";
+import { Constants } from "expo-constants";
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
 import CampsiteInfo from "./CampsiteInfoComponent";
-import { CAMPSITES } from "../shared/campsites";
+
+const DirectoryNavigator = createStackNavigator(
+  {
+      Directory: { screen: Directory },
+      CampsiteInfo: { screen: CampsiteInfo }
+  }, 
+  {
+      initialRouteName: 'Directory',
+      defaultNavigationOptions: {
+          headerStyle: {
+              backgroundColor: '#5637DD'
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+              color: '#fff'
+          }
+      }
+  }
+);
+
+const AppNavigator = createAppContainer(DirectoryNavigator);
 
 class Main extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      campsites: CAMPSITES,
-      selectedCampsite: null,
-    };
-  }
-
-  onCampsiteSelect(campsiteId) {
-    this.setState({
-      selectedCampsite: campsiteId,
-    });
-  }
-
   render() {
-    return (
-      <View style={{flex: 1}}>
-        <Directory
-          campsites={this.state.campsites}
-          onPress={(campsiteId) => this.onCampsiteSelect(campsiteId)}
-        ></Directory>
-        <CampsiteInfo
-          campsite={
-            this.state.campsites.filter(
-              (campsite) => campsite.id === this.state.selectedCampsite
-            )[0]
-          }
-        ></CampsiteInfo>
-      </View>
-    );
+      return (
+          <View
+              style={{
+                  flex: 1,
+                  // paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight
+          }}>
+              <AppNavigator />
+          </View>
+      );
   }
 }
 

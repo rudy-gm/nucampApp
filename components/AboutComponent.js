a9aad9065905745dea7492bf0af909198fa7c348
rodolfo-gm <gutierrezrodolfo80@gmail.com>
2022-04-13 21:32:48 -0400

    9th commit // Redux Adding Favorites

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -2,8 +2,15 @@ import React, { Component } from "react";
 import { ListItem } from "react-native-elements";
 import { Text, ScrollView, FlatList } from "react-native";
 import { Card } from "react-native-elements";
-import { PARTNERS } from "../shared/partners";
+import { connect } from "react-redux";
+import { baseUrl } from "../shared/baseUrl";
+import Loading from "./LoadingComponent";
 
+const mapStateToProps = (state) => {
+  return {
+    partners: state.partners,
+  };
+};
 
 function Mission() {
   return (
@@ -22,14 +29,6 @@ function Mission() {
 }
 
 class About extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      partners: PARTNERS,
-    };
-  }
-
   static navigationOptions = {
     title: "About Us",
   };
@@ -40,21 +39,42 @@ class About extends Component {
         <ListItem
           title={item.name}
           subtitle={item.description}
-          leftAvatar={{ source: require('./images/bootstrap-logo.png'
-          ) }}
-          
+          leftAvatar={{ source: { uri: baseUrl + item.image } }}
         ></ListItem>
       );
     };
 
+
+    if(this.props.partners.isLoading){
+
+      return(
+        <ScrollView>
+          <Mission></Mission>
+          <Card title="Community Partners">
+            <Loading></Loading>
+          </Card>
+      </ScrollView>
+      )
+    }
+
+    if(this.props.partners.errMess){
+      return(
+        <ScrollView>
+          <Mission></Mission>
+          <Card title="Community Partners">
+            <Text>{this.props.partners.errMess}</Text>
+          </Card>
+      </ScrollView>
+      )
+    }
     return (
       <ScrollView>
         <Mission></Mission>
         <Card title="Community Partners">
           <FlatList
-             data={this.state.partners}
-             renderItem={renderPartner}
-             keyExtractor={(item) => item.id.toString()}
+            data={this.props.partners.partners}
+            renderItem={renderPartner}
+            keyExtractor={(item) => item.id.toString()}
           ></FlatList>
         </Card>
       </ScrollView>
@@ -62,4 +82,4 @@ class About extends Component {
   }
 }
 
-export default About;
+export default connect(mapStateToProps)(About);
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,17 +1,38 @@
 import React, { Component } from "react";
-import { View, Platform, StyleSheet, Text, ScrollView, Image } from "react-native";
+import {
+  View,
+  Platform,
+  StyleSheet,
+  Text,
+  ScrollView,
+  Image,
+} from "react-native";
 import { createStackNavigator } from "react-navigation-stack";
 import { createBottomTabNavigator } from "react-navigation-tabs";
 import { createAppContainer } from "react-navigation";
-import { Constants } from "expo-constants";
+import  Constants  from "expo-constants";
 import CampsiteInfo from "./CampsiteInfoComponent";
 import { createDrawerNavigator, DrawerItems } from "react-navigation-drawer";
 import { Icon } from "react-native-elements";
-import SafeAreaView  from "react-native-safe-area-view";
+import SafeAreaView from "react-native-safe-area-view";
+import { connect } from "react-redux";
+import {
+  fetchCampsites,
+  fetchComments,
+  fetchPartners,
+  fetchPromotions,
+} from "../redux/ActionCreators";
 import Home from "./HomeComponent";
 import Directory from "./DirectoryComponent";
 import Contact from "./ContactComponent";
 import About from "./AboutComponent";
+
+const mapDispatchToProps ={
+  fetchCampsites,
+  fetchComments,
+  fetchPartners,
+  fetchPromotions
+};
 
 const DirectoryNavigator = createStackNavigator(
   {
@@ -119,96 +140,109 @@ const AboutNavigator = createStackNavigator(
   }
 );
 
-const CustomDrawerContentComponent = props =>(
+const CustomDrawerContentComponent = (props) => (
   <ScrollView>
     <SafeAreaView
       style={styles.container}
-      forceInset={{top:'always', horizontal: 'never'}}
-
-     
+      forceInset={{ top: "always", horizontal: "never" }}
     >
       <View style={styles.drawerHeader}>
-        <View style={{flex: 1}}>
+        <View style={{ flex: 1 }}>
           <Image
-            source={require('./images/logo.png')}
+            source={require("./images/logo.png")}
             style={styles.drawerImage}
-            ></Image>
+          ></Image>
         </View>
-        <View style={{flex: 2}}>
+        <View style={{ flex: 2 }}>
           <Text style={styles.drawerHeaderText}>NuCamp</Text>
         </View>
       </View>
 
       <DrawerItems {...props}></DrawerItems>
-
     </SafeAreaView>
   </ScrollView>
 );
 
 const MainNavigator = createDrawerNavigator(
   {
-    Home: { screen: HomeNavigator,
-            navigationOptions:{
-              drawerIcon:({tintColor}) =>(
-                <Icon
-                  name='home'
-                  type="font-awesome"
-                  size={24}
-                  color={tintColor}
-                  ></Icon>
-              )
-            } },
-    Directory: { screen: DirectoryNavigator,
-      navigationOptions:{
-        drawerIcon:({tintColor}) =>(
+    Home: {
+      screen: HomeNavigator,
+      navigationOptions: {
+        drawerIcon: ({ tintColor }) => (
           <Icon
-            name='list'
+            name="home"
             type="font-awesome"
             size={24}
             color={tintColor}
-            ></Icon>
-        )
-      } },
-    About: { screen: AboutNavigator,
-      navigationOptions:{
-        drawerLabel: 'About Us',
-        drawerIcon:({tintColor}) =>(
+          ></Icon>
+        ),
+      },
+    },
+    Directory: {
+      screen: DirectoryNavigator,
+      navigationOptions: {
+        drawerIcon: ({ tintColor }) => (
           <Icon
-            name='info-circle'
+            name="list"
             type="font-awesome"
             size={24}
             color={tintColor}
-            ></Icon>
-        )
-      } },
-    Contact: { screen: ContactNavigator,
-      navigationOptions:{
+          ></Icon>
+        ),
+      },
+    },
+    About: {
+      screen: AboutNavigator,
+      navigationOptions: {
+        drawerLabel: "About Us",
+        drawerIcon: ({ tintColor }) => (
+          <Icon
+            name="info-circle"
+            type="font-awesome"
+            size={24}
+            color={tintColor}
+          ></Icon>
+        ),
+      },
+    },
+    Contact: {
+      screen: ContactNavigator,
+      navigationOptions: {
         drawerLabel: "Contact Us",
-        drawerIcon:({tintColor}) =>(
+        drawerIcon: ({ tintColor }) => (
           <Icon
-            name='addres-card'
+            name="addres-card"
             type="font-awesome"
             size={24}
             color={tintColor}
-            ></Icon>
-        )
-      } },
+          ></Icon>
+        ),
+      },
+    },
   },
   {
     drawerBackgroundColor: "#CEC8FF",
-    contentComponent: CustomDrawerContentComponent
+    contentComponent: CustomDrawerContentComponent,
   }
 );
 
 const AppNavigator = createAppContainer(MainNavigator);
 
 class Main extends Component {
+
+  componentDidMount(){
+    this.props.fetchCampsites();
+    this.props.fetchComments();
+    this.props.fetchPromotions();
+    this.props.fetchPartners();
+  }
+
   render() {
     return (
       <View
         style={{
           flex: 1,
-          //  paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight
+           paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight
         }}
       >
         <AppNavigator />
@@ -218,32 +252,32 @@ class Main extends Component {
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex: 1
-  },
-  drawerHeader:{
-    backgroundColor: '#5637DD',
-    height: 140,
-    alignItems: 'center',
-    justifyContent: 'center',
+  container: {
     flex: 1,
-    flexDirection: 'row'
   },
-  drawerHeaderText:{
-    color:'#fff',
+  drawerHeader: {
+    backgroundColor: "#5637DD",
+    height: 140,
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
+    flexDirection: "row",
+  },
+  drawerHeaderText: {
+    color: "#fff",
     fontSize: 24,
-    fontWeight: 'bold'
+    fontWeight: "bold",
   },
-  drawerImage:{
+  drawerImage: {
     margin: 10,
-    height:60,
-    width:60
+    height: 60,
+    width: 60,
   },
-  stackIcon:{
-    marginLeft:10, 
-    color: '#fff',
-    fontSize: 24
-  }
-})
+  stackIcon: {
+    marginLeft: 10,
+    color: "#fff",
+    fontSize: 24,
+  },
+});
 
-export default Main;
+export default connect (null, mapDispatchToProps)(Main);

6420e5aca96c5fd12623598e01dbacd29bf594de
rodolfo-gm <gutierrezrodolfo80@gmail.com>
2022-04-19 00:26:37 -0400

    14th commit // Swipe and Delete Favorites

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -186,6 +186,10 @@ export const addComment = (comment) =>({
 
     type: ActionTypes.ADD_COMMENT,
     payload: comment
-})
+}); 
 
+export const deleteFavorite = campsiteId =>({
+  type: ActionTypes.DELETE_FAVORITE,
+  payload: campsiteId
 
+});

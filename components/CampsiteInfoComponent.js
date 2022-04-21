51a8c0d96cda21563a846cb51897e11a9904fe7d
rodolfo-gm <gutierrezrodolfo80@gmail.com>
2022-04-20 20:55:56 -0400

    18th commit // gestures part 2

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -34,11 +34,20 @@ const mapDispatchToProps = {
 function RenderCampsite(props) {
   const { campsite } = props;
 
+  const view = React.createRef();
+
+
   const recognizeDrag =({dx}) =>(dx < -200) ? true: false;
 
   const panResponder = PanResponder.create(
     {
       onStartShouldSetPanResponder: () => true,
+      onPanResponderGrant: () => {
+        view.current.rubberBand(1000)
+        .then(
+          endState => console.log(endState.finished ? 'finished' : 'canceled')
+        )
+      },
       onPanResponderEnd: (e,gestureState) => {
         console.log('pan responder', gestureState);
         if(recognizeDrag(gestureState)){
@@ -73,6 +82,7 @@ function RenderCampsite(props) {
       animation="fadeInDown" 
       duration={2000} 
       delay={1000}
+      ref={view}
       {...panResponder.panHandlers}
       >
         <Card
